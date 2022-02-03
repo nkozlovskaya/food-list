09cbc683fb04b51c3aafb61e2927c1edb58b142a
nadezhda <kozlovskayani@yandex.ru>
2022-02-03 10:37:31 +0300

    create pages with meal-list and category

---
+++ src/components/Footer.jsx
@@ -0,0 +1,20 @@
+function Footer() {
+  return (
+    <footer className="page-footer teal accent-2">
+      <div className="footer-copyright">
+        <div className="container">
+          © {new Date().getFullYear()} Copyright Text
+          <a
+            className="grey-text text-lighten-4 right"
+            href="https://github.io/nkozlovskaya/react-shop"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Repo
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+}
+export { Footer };
